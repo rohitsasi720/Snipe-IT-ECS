1c74657c072521479143cae231c682b08bced772
Rohit <rohitsasi720@gmail.com>
2022-12-08 22:09:39 +0530

    update

--- terraform_with_ALB/c1-version.tf
+++ terraform_with_ALB/c1-version.tf
@@ -3,11 +3,21 @@ provider "aws" {
 #   profile = default
   
 }
+
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "4.33.0"
     }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "snipe-it-terra"
+    key    = "path/tf.state"
+    region =  "us-east-1"
+    encrypt = true
   }
 }
